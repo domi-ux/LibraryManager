370be2a54458ef7992580fe60c536c2fbff0f0fc
domi-ux <modometudi@gmail.com>
2025-10-15 11:57:46 +0000

    Update Livre.java
    Ajout du projet Java initial : Livre et Main

--- src/library/Livre.java
+++ src/library/Livre.java
@@ -3,18 +3,18 @@ package library;
 	//New modif 1
 public class Livre {
 	private String titre;
-	private String auteur;
+	private String ecrivain;
 	private boolean emprunte;
 	public Livre(String titre, String auteur) {
 		this.titre = titre;
-		this.auteur = auteur;
+		this.ecrivain = auteur;
 		this.emprunte = false;
 		}
 	public void emprunter() { emprunte = true; }
 	public void retourner() { emprunte = false; }
 	@Override
 	public String toString() {
-		return titre + " - " + auteur + (emprunte ? " (emprunté)" : "");
+		return titre + " - " + ecrivain + (emprunte ? " (emprunté)" : "");
 		}
 
 }
